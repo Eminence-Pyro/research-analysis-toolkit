ec81bceabb569d7921fb6c6688a90a7207c5309f
Eminence-Pyro <divinennata@gmail.com>
2026-07-10 17:36:50 +0100

    structure: exporters package init

---
+++ research_engine/exporters/__init__.py
@@ -0,0 +1,7 @@
+"""
+research_engine/exporters/
+Writes domain objects and analysis results to external file formats.
+
+Depends on: research_engine/models/, research_engine/analysis/
+Does NOT depend on: parsers, generators, validators, reports
+"""
