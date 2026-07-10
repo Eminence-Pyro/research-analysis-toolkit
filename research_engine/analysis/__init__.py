05df890d40a0b09d0c89a6355c86ab0790864fee
Eminence-Pyro <divinennata@gmail.com>
2026-07-10 17:36:46 +0100

    structure: analysis package init

---
+++ research_engine/analysis/__init__.py
@@ -0,0 +1,7 @@
+"""
+research_engine/analysis/
+Computes statistics on Dataset and domain objects.
+
+Depends on: research_engine/models/
+Does NOT depend on: parsers, generators, validators, exporters, reports
+"""
